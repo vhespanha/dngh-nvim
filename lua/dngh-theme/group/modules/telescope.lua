892a8dddb58ca1701b37889919cbf304867a7b38
vhespanha <vhespanha@tuta.io>
2024-07-27 20:48:24 -0300

    normal caret hl for telescope

--- lua/dngh-theme/group/modules/telescope.lua
+++ lua/dngh-theme/group/modules/telescope.lua
@@ -5,7 +5,7 @@ local M = {}
 function M.get(spec, config, opts)
   -- stylua: ignore
   return {
-    TelescopeSelectionCaret = { fg = spec.palette.border.muted },
+    TelescopeSelectionCaret = { fg = spec.palette.fg.default },
     TelescopeSelection      = { link = 'CursorLine' },
     TelescopeMatching       = { link = 'Search' },
     TelescopeBorder         = { fg = spec.palette.border.muted },
